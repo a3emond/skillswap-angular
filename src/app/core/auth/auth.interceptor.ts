7c71769d5fd1afc08e3337588b4673789b9fdf5d
Lililatortue <will.descoteaux@gmail.com>
2026-03-08 20:05:53 -0400

    added the changes asked

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,12 +1,23 @@
 import { HttpInterceptorFn }      from "@angular/common/http";
 import { inject }                 from "@angular/core";
+import { MatSnackBar }            from '@angular/material/snack-bar';
 import { Router }                 from "@angular/router";
 import { catchError, throwError } from 'rxjs';
 import { AuthStore }              from "../auth/auth.store"
+import { InternalErrorException } from "./internal-error.exception";
 
+
+
+
+ /*
+  *
+  *
+  *
+  */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const store = inject(AuthStore);
-  const router= inject(Router)
+  const router= inject(Router);
+  const snackBar: MatSnackBar = inject(MatSnackBar);
 
   //set header
   if(store.isAuthenticated()) {
@@ -21,13 +32,16 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       catchError((error) => {
         if (error.status === 401) {
           //clear session
-          store.logout();
+          store.ClearSession();
 
+          //diff log mechanism
+          console.log("user session is expired");
+          snackBar.open('Session has expire pls login again', 'Close', { duration: 3000 });
           //navigate to login
           router.navigate(["/login"], {"queryParams":{returnUrl: router.url} })
         }
 
-        return throwError(()=> error)
+        return throwError(()=> new InternalErrorException("something went wrong pls try again later"))
       })
     );
 }
