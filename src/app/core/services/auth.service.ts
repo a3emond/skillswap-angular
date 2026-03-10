7bfd5eed21edd28dd208d30f75a6f250612bb59c
Lililatortue <will.descoteaux@gmail.com>
2026-03-09 22:19:13 -0400

    impl du register service, IMPORTANT: jai mis les errors dans le meme files

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,27 +3,10 @@ import { inject, Injectable } from '@angular/core';
 import { catchError, of, throwError } from 'rxjs';
 import { ApiClient } from '../http/api-client';
 import { RegisterDto } from '../models/dto/register.dto';
+import { ApiError } from '../http/api-error.model';
 
-/**
-    MODULE AUTHENTIFICATION
-    POST /auth/register
-    Crée un nouveau compte utilisateur.
-    Champs obligatoires :
-        • name
-        • username
-        • email
-        • password
-        • bio
-        • skills (tableau)
-    Succès (201)
-    Retourne un message et l’objet utilisateur créé.
-    Erreurs possibles :
-      400 – Missing required fields
-      400 – Invalid username
-      409 – Email already in use
-      409 – Username already in use (retourne suggested_username)
-    Votre frontend doit afficher correctement le nom d’utilisateur suggéré si présent.
- */
+
+
 
 
 @Injectable({providedIn: 'root'})
@@ -33,61 +16,42 @@ export class AuthService {
     Register(dto: RegisterDto) {
         return this.#http.post('/auth/register',dto)
         .pipe(
-            catchError((err: any) => {
-
-                let error: any;
-                switch(err.status) {
-                  case 405:
-                      error = err.body.contains("username")
-                          ? new InvalidUsername()
-                          : new MissingRequiredFields()
-                      break;
-
-                  case 409:
-                      error = err.body == null
-                          ? new EmailAlreadyInUse()
-                          : new UsernameAlreadyInUse(err.body)
-                      break;
-
-                  default: throw new Error("Internal Error");
-                }
-
-                return throwError(()=> error);
+            catchError((err: ApiError) => {
+                console.error("Error sent: ", err);
+                return throwError(()=> err);
             })
         );
     }
 }
 
-//400
-class MissingRequiredFields extends Error {
-  constructor(message: string = "Missing required fields") {
-    super(message);
-    this.name = 'MissingRequiredFields';
-    Object.setPrototypeOf(this, MissingRequiredFields.prototype);
-  }
-}
-class InvalidUsername extends Error {
-  constructor(message: string = "InvalidUsername") {
-    super(message);
-    this.name = 'InvalidUsername';
-    Object.setPrototypeOf(this, InvalidUsername.prototype);
-  }
+/*
+  Erreurs possibles :
+      400 – Missing required fields
+      400 – Invalid username
+      409 – Email already in use
+      409 – Username already in use (retourne suggested_username)
+*/
+
+export type MissingRequiredFields = ApiError & {
+    status : 400,
+    message: "missing required fields"
 }
 
-//409 special error
-class EmailAlreadyInUse extends Error {
-  constructor(message: string = "Email already in use") {
-    super(message);
-    this.name = 'EmailAlreadyInUse';
-    Object.setPrototypeOf(this, EmailAlreadyInUse.prototype);
-  }
+export type InvalidUsername = ApiError & {
+    status : 400,
+    message: "Invalid ",
+
 }
-class UsernameAlreadyInUse extends Error {
-  constructor(body: string, message: string = "Username already in use") {
-    super(message);
-    this.name = 'UsernameAlreadyInUse';
-    Object.setPrototypeOf(this, UsernameAlreadyInUse.prototype);
-  }
+
+//409
+export type EmailAlreadyInUse = ApiError & {
+    status : 409,
+    message: "Email is already in use",
+}
+export type UsernameAlreadyInUse = ApiError & {
+    status : 409,
+    message: "username is already in use",
+    suggested_username: String
 }
 
 
