80990d4a5dae410c1ab8ed7c2d82be190a9b8c51
Lililatortue <will.descoteaux@gmail.com>
2026-03-10 15:02:51 -0400

    rajouter la route et enlever le boilerplate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './core/signup.component/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+  path: 'signup',
+  component: SignupComponent
+}
+
+];
