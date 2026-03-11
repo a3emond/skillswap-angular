660d2251f7c2b01112b79fc2e12d4091acd79e1a
Lililatortue <will.descoteaux@gmail.com>
2026-03-10 22:03:28 -0400

    remove nested mess

--- src/app/core/services/error/expired-token-error.model.ts
+++
@@ -1,6 +0,0 @@
-import { ApiError } from "../../http/api-error.model";
-
-export type ExpiredTokenError = ApiError & {
-    status: 401,
-    message: "Token has expired"
-}
--- src/app/core/services/error/forbidden-error.model.ts
+++
@@ -1,6 +0,0 @@
-import { ApiError } from "../../http/api-error.model";
-
-export type ForbiddenError = ApiError & {
-    status: 403,
-    message: "Forbidden"
-}
