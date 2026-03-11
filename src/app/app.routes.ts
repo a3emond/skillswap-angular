2468bd0b413bbb55701b022fa0235caac7e04911
Lililatortue <will.descoteaux@gmail.com>
2026-03-11 13:02:15 -0400

    added Dashboard component && made login its child

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './component/login/login';
+import { Dashboard } from './component/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: "",
+      component: Dashboard,
+      children: [
+          {path: 'login',component: Login }
+      ]
+    }
+];
